d9764db0da393ba5e871db082d234a2be2e7d0e0
Alex Borges <alexborgessjc@hotmail.com>
2022-02-28 13:51:22 -0300

    Envio do exercício 1

---
+++ ex1/src/ClassLibrary1/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassLibrary1
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private DateTime _datanascimento;
+        private Double _altura;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; } 
+        }
+
+        public DateTime DataNascimento
+        {
+            get { return _datanascimento; }
+            set { _datanascimento = value; }
+        }
+
+        public Double Altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+    }
+}
---
+++ ex1/src/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+
+Console.WriteLine("Exercício 1");
+Console.WriteLine("-----------------------");
+try
+{
+    Pessoa pessoa1 = new Pessoa();
+    pessoa1.Nome = "Alex";
+    pessoa1.DataNascimento = new DateTime(1990, 1, 20);
+    pessoa1.Altura = 1.76;
+
+    Console.WriteLine($"O nome da pessoa é: {pessoa1.Nome}");
+    Console.WriteLine($"A data de nascimento é: {pessoa1.DataNascimento.ToString("d")}");
+    Console.WriteLine($"A altura da pessoa é: {pessoa1.Altura}");    
+}
+catch (Exception ex)
+{
+    Console.WriteLine("-----------------------");
+    Console.WriteLine(ex.Message);
+}
