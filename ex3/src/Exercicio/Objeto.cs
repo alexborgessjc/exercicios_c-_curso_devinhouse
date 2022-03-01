f17d322aa4dba710c3b553cab836c2bd2bd20f23
Alex Borges <alexborgessjc@hotmail.com>
2022-02-28 22:02:44 -0300

    Envio do exercício 3

---
+++ ex3/src/Exercicio/Objeto.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio
+{
+    public class Objeto
+    {
+        private int _altura;
+        private int _base;
+        private double _area;
+
+        public int Altura { get => _altura; set => _altura = value; }
+        public int Base { get => _base; set => _base = value; }
+        public double Area { get => _area; set => _area = value; }
+               
+        public void RetornaPropriedades()
+        {
+            Console.WriteLine("------Propriedades------");
+            Console.WriteLine($"O valor da altura é: {Altura}");
+            Console.WriteLine($"O valor da base é: {Base}");
+            Console.WriteLine($"O valor da área é: {Area}");
+        }
+    }
+}
---
+++ ex3/src/Sistema/Program.cs
@@ -0,0 +1,22 @@
+﻿using Exercicio;
+
+try
+{
+    Console.WriteLine("Entre com o valor da base!");    
+    int valorbase = Int16.Parse(Console.ReadLine());
+    Console.WriteLine("Entre com o valor da altura!");
+    int valoraltura = Int16.Parse(Console.ReadLine());
+
+    Objeto objeto1 = new();
+    objeto1.Base = valorbase;
+    objeto1.Altura = valoraltura;
+    objeto1.Area = objeto1.Base * objeto1.Altura / 2;
+
+    //objeto1.CalcularArea(valoraltura,valorbase);
+    objeto1.RetornaPropriedades();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("-----------------------");
+    Console.WriteLine(ex.Message);
+}
