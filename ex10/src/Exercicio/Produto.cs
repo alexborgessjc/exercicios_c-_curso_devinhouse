9761760fd26f5f20ff7f6ae99495db55b224d1e4
Alex Borges <alexborgessjc@hotmail.com>
2022-03-16 12:39:05 -0300

    Envio do exercício 10

---
+++ ex10/src/Exercicio/Produto.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio
+{
+    public class Produto
+    {
+        private string _nomedoproduto;
+        private double _valor;
+        private double _valordodesconto;
+
+        public string NomeDoProduto { get => _nomedoproduto; set => _nomedoproduto = value; }
+        public double ValorDoProduto { get => _valor; set => _valor = value; }
+        public double ValorDoDesconto { get => _valordodesconto; set => _valordodesconto = value; }
+        public void RetornaValorDoDesconto()
+        {         
+
+            double valorfinal = ValorDoProduto * (ValorDoDesconto / 100);
+                                  
+            Console.WriteLine($"O valor de desconto é: {valorfinal}");
+        }
+        public void RetornaValorFinal()
+        {
+
+            double valordodesconto = ValorDoProduto * (ValorDoDesconto / 100);
+
+            double valorfinal = ValorDoProduto - valordodesconto;
+
+            Console.WriteLine($"O valor final do produto é: {valorfinal}");
+        }
+    }
+}
---
+++ ex10/src/Sistema/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercicio;
+
+Console.WriteLine("Exercício 9");
+Console.WriteLine("-----------------------");
+
+try
+{
+    Produto produto1 = new Produto();
+    produto1.NomeDoProduto = "ProdutoA";
+    produto1.ValorDoProduto = 100;
+    produto1.ValorDoDesconto = 20;
+
+    produto1.RetornaValorFinal();
+    produto1.RetornaValorDoDesconto();  
+}
+catch (Exception ex)
+{
+    Console.WriteLine("-----------------------");
+    Console.WriteLine(ex.Message);
+}
